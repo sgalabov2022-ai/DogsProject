6cd95d29ab29f0e724661166123801e860039a19
unknown <v.notev2021@pmg-pernik.com>
2026-02-06 11:12:36 +0200

    adding vs project

---
+++ DogsProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DogsProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
